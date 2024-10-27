353d256755eef170923ab2ee3c842da325e9f0d1
PandolfiJoaquin <joaquin.pandolfi@despegar.com>
2024-10-27 17:44:45 -0300

    [FEATURE] Read port from env var

--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"log"
 	"messages/src/router"
+	"os"
 )
 
 const PORT = ":8080"
@@ -16,7 +17,7 @@ func main() {
 	}
 
 	log.Println("Starting at port ", PORT)
-	if err = r.Run(PORT); err != nil {
+	if err = r.Run("0.0.0.0:" + os.Getenv("PORT")); err != nil {
 		log.Fatalln("Error starting server: ", err)
 	}
 
