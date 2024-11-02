734647393ff66c2dfb340d0b4de1155087f318ae
PandolfiJoaquin <joaquin.pandolfi@despegar.com>
2024-11-02 19:20:02 -0300

    [FEATURE] Use test database when outside of heroku

--- server/src/controller/notifications_test.go
+++ server/src/controller/notifications_test.go
@@ -56,5 +56,4 @@ func TestAddDeviceForUser(t *testing.T) {
 	//assert
 	usersConnectorMock.AssertCalled(t, "CheckUserExists", "userId", bearerToken)
 	devicesDatabaseMock.AssertCalled(t, "AddDevice", "userId", "deviceToken")
-
 }
--- server/src/repository/notifications.go
+++ server/src/repository/notifications.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/jmoiron/sqlx"
 	"log"
+	"os"
 )
 
 type DevicesDatabaseInterface interface {
@@ -22,13 +23,20 @@ func NewDevicesPersistentDatabase(db *sqlx.DB) (DevicesDatabaseInterface, error)
 
 func prepareDatabase(db *sqlx.DB) error {
 	//create notifications_db if it doesn't exist
-	_, err := db.Exec(`
-		CREATE TABLE IF NOT EXISTS devices_db (
+	query := `
+		CREATE TABLE IF NOT EXISTS devices_db`
+	if os.Getenv("ENVIRONMENT") != "HEROKU" {
+		query += `_test`
+	}
+
+	query += `(
 		    			user_id TEXT,
 		    			token TEXT,
 		    			PRIMARY KEY (user_id, token)
 		);
-	`)
+	`
+	log.Println("Executing query: ", query)
+	_, err := db.Exec(query)
 	if err != nil {
 		return err
 	}
